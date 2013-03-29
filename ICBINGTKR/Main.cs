93917fb177ecae94c63ee63ff4a89c4de0d78241
Kallikrates <kallikrates.koios@gmail.com>
2013-03-28 20:49:16 -0400

    Array constructor for IntVec3
    (Probably worthless but whatever)

--- ICBINGTKR/Main.cs
+++ ICBINGTKR/Main.cs
@@ -82,6 +82,7 @@ namespace ICBINGTKR
 		public int x;
 		public int y;
 		public int z;
+		public IntVec3(int[] arry) : this(arry[0], arry[1], arry[2]) {}
 		public IntVec3(int x, int y, int z){
 			this.x = x;
 			this.y = y;
