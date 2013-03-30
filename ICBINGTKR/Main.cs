6eb76f6c88aa4817597bccf48e50c1a2d8ad3545
Kallikrates <kallikrates.koios@gmail.com>
2013-03-30 04:50:53 -0400

    Fixed some code, Enhanced the example code.

--- ICBINGTKR/Main.cs
+++ ICBINGTKR/Main.cs
@@ -7,6 +7,7 @@ namespace ICBINGTKR
 		public static void Main (string[] args) {
 			Map testmap = new Map("generation.map");
 			testmap.NewBrush(new IntVec3(64,64,64), new IntVec3(512,512,512)).AddCuttingPlane(new IntVec3(64,64,256), new IntVec3(256,64,64), new IntVec3(64,256,64));
+			testmap.NewBrush (new IntVec3(512,64,256), new IntVec3(1024,512,512));
 			WriteMap(testmap);
 		}
 
