cd9485613c90da98568cd0286012d14dea1413aa
Ed Halferty <edhalferty@gmail.com>
2013-05-07 18:39:38 -0400

    Just messing around

---
+++ ICBINGTKR/ATypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// This contains multi-Brush "assembly" types that can be used to build a map.
+/// </summary>
+
+namespace ICBINGTKR
+{
+    class ATypes
+    {
+        class HollowBox
+        {
+        }
+    }
+}
