9c2a78cd97d39e98a68ec42ac57fb1f90b15496a
Ed Halferty <edhalferty@gmail.com>
2013-05-08 13:54:12 -0400

    Just messing around

--- ICBINGTKR/ATypes.cs
+++ ICBINGTKR/ATypes.cs
@@ -9,12 +9,62 @@ using System.Text;
 
 namespace ICBINGTKR
 {
-    class ATypes
+    abstract class AssemblyGenerator
     {
-        class HollowBox
+        protected List<Brush> brushes = new List<Brush>();
+
+        public IEnumerable<Brush> Brushes { get { return this.brushes; } }
+    }
+
+    class HollowBoxGenerator : AssemblyGenerator
+    {
+        public HollowBoxGenerator(IntVec3 origin, IntVec3 dimensions, int wallThickness, Texture tex = null)
         {
-            public HollowBox(IntVec3 origin, IntVec3 dimensions, int thickness)
+            if (tex == null)
             {
+                tex = Brush.defaultTexture;
+            }
+
+            // Top
+            {
+                var veca = new IntVec3(origin.x - (dimensions.x / 2), origin.y - (dimensions.y / 2), origin.z + (dimensions.z / 2) - wallThickness);
+                var vecb = new IntVec3(origin.x + (dimensions.x / 2), origin.y + (dimensions.y / 2), origin.z + (dimensions.z / 2));
+                brushes.Add(new Brush(veca, vecb, tex));
+            }
+
+            // Bottom
+            {
+                var veca = new IntVec3(origin.x - (dimensions.x / 2), origin.y - (dimensions.y / 2), origin.z - (dimensions.z / 2));
+                var vecb = new IntVec3(origin.x + (dimensions.x / 2), origin.y + (dimensions.y / 2), origin.z - (dimensions.z / 2) + wallThickness);
+                brushes.Add(new Brush(veca, vecb, tex));
+            }
+
+            // Side +x
+            {
+                var veca = new IntVec3(origin.x + (dimensions.x / 2) - wallThickness, origin.y - (dimensions.y / 2), origin.z - (dimensions.z / 2));
+                var vecb = new IntVec3(origin.x + (dimensions.x / 2), origin.y + (dimensions.y / 2), origin.z + (dimensions.z / 2));
+                brushes.Add(new Brush(veca, vecb, tex));
+            }
+
+            // Side -x
+            {
+                var veca = new IntVec3(origin.x - (dimensions.x / 2), origin.y - (dimensions.y / 2), origin.z - (dimensions.z / 2));
+                var vecb = new IntVec3(origin.x - (dimensions.x / 2) + wallThickness, origin.y + (dimensions.y / 2), origin.z + (dimensions.z / 2));
+                brushes.Add(new Brush(veca, vecb, tex));
+            }
+
+            // Side +y
+            {
+                var veca = new IntVec3(origin.x - (dimensions.x / 2), origin.y + (dimensions.y / 2) - wallThickness, origin.z - (dimensions.z / 2));
+                var vecb = new IntVec3(origin.x + (dimensions.x / 2), origin.y + (dimensions.y / 2), origin.z + (dimensions.z / 2));
+                brushes.Add(new Brush(veca, vecb, tex));
+            }
+
+            // Side -y
+            {
+                var veca = new IntVec3(origin.x - (dimensions.x / 2), origin.y - (dimensions.y / 2), origin.z - (dimensions.z / 2));
+                var vecb = new IntVec3(origin.x + (dimensions.x / 2), origin.y - (dimensions.y / 2) + wallThickness, origin.z + (dimensions.z / 2));
+                brushes.Add(new Brush(veca, vecb, tex));
             }
         }
     }
