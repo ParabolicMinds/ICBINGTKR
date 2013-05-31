f5c0808d65e969525395fab45fb9cf74c782c5e7
Ed Halferty <edhalferty@gmail.com>
2013-05-31 18:41:05 -0400

    Refactored some stuff to make way for terrain and room generators.

--- ICBINGTKR/ATypes.cs
+++ ICBINGTKR/ATypes.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 
-
 /// <summary>
 /// This contains multi-Brush "assembly" types that can be used to build a map.
 /// </summary>
@@ -18,15 +17,37 @@ namespace ICBINGTKR
         public List<Brush> Brushes { get { return this.brushes; } }
     }
 
+    class RightHexahedralBrushGenerator : AssemblyGenerator
+    {
+        private IntVec3 spoint, epoint;
+
+        public RightHexahedralBrushGenerator(IntVec3 veca, IntVec3 vecb)
+            : this(veca, vecb, BrushFace.DEFAULT_TEXTURE) { }
+        public RightHexahedralBrushGenerator(IntVec3 veca, IntVec3 vecb, Texture texture)
+        {
+            this.spoint = veca;
+            this.epoint = vecb;
+            Utils.fixDirection(ref this.spoint, ref this.epoint);
+
+            Brush b = new Brush();
+
+            b.AddFace(new BrushFace(new IntPlane(this.spoint.x, 0, 0, this.spoint.x, 1, 0, this.spoint.x, 0, 1), texture));
+            b.AddFace(new BrushFace(new IntPlane(this.epoint.x, 0, 0, this.epoint.x, 0, 1, this.epoint.x, 1, 0), texture));
+            b.AddFace(new BrushFace(new IntPlane(0, this.spoint.y, 0, 0, this.spoint.y, 1, 1, this.spoint.y, 0), texture));
+            b.AddFace(new BrushFace(new IntPlane(0, this.epoint.y, 0, 1, this.epoint.y, 0, 0, this.epoint.y, 1), texture));
+            b.AddFace(new BrushFace(new IntPlane(0, 0, this.spoint.z, 1, 0, this.spoint.z, 0, 1, this.spoint.z), texture));
+            b.AddFace(new BrushFace(new IntPlane(0, 0, this.epoint.z, 0, 1, this.epoint.z, 1, 0, this.epoint.z), texture));
+            
+            brushes.Add(b);
+        }
+    }
+
     class HollowBoxGenerator : AssemblyGenerator
     {
-        public HollowBoxGenerator(IntVec3 origin, IntVec3 dimensions, int wallThickness, Texture tex = null)
+        public HollowBoxGenerator(IntVec3 origin, IntVec3 dimensions, int wallThickness)
+            : this(origin, dimensions, wallThickness, BrushFace.DEFAULT_TEXTURE) { }
+        public HollowBoxGenerator(IntVec3 origin, IntVec3 dimensions, int wallThickness, Texture texture)
         {
-            if (tex == null)
-            {
-                tex = Brush.defaultTexture;
-            }
-
             // Top
             {
                 var veca = new IntVec3(
@@ -37,7 +58,7 @@ namespace ICBINGTKR
                     origin.x + (dimensions.x / 2),
                     origin.y + (dimensions.y / 2),
                     origin.z + (dimensions.z / 2));
-                brushes.Add(new Brush(veca, vecb, tex));
+                brushes.AddRange(new RightHexahedralBrushGenerator(veca, vecb, texture).Brushes);
             }
 
             // Bottom
@@ -50,7 +71,7 @@ namespace ICBINGTKR
                     origin.x + (dimensions.x / 2),
                     origin.y + (dimensions.y / 2),
                     origin.z - (dimensions.z / 2) + wallThickness);
-                brushes.Add(new Brush(veca, vecb, tex));
+                brushes.AddRange(new RightHexahedralBrushGenerator(veca, vecb, texture).Brushes);
             }
 
             // Side +x
@@ -63,7 +84,7 @@ namespace ICBINGTKR
                     origin.x + (dimensions.x / 2),
                     origin.y + (dimensions.y / 2),
                     origin.z + (dimensions.z / 2));
-                brushes.Add(new Brush(veca, vecb, tex));
+                brushes.AddRange(new RightHexahedralBrushGenerator(veca, vecb, texture).Brushes);
             }
 
             // Side -x
@@ -76,7 +97,7 @@ namespace ICBINGTKR
                     origin.x - (dimensions.x / 2) + wallThickness,
                     origin.y + (dimensions.y / 2),
                     origin.z + (dimensions.z / 2));
-                brushes.Add(new Brush(veca, vecb, tex));
+                brushes.AddRange(new RightHexahedralBrushGenerator(veca, vecb, texture).Brushes);
             }
 
             // Side +y
@@ -89,7 +110,7 @@ namespace ICBINGTKR
                     origin.x + (dimensions.x / 2),
                     origin.y + (dimensions.y / 2),
                     origin.z + (dimensions.z / 2));
-                brushes.Add(new Brush(veca, vecb, tex));
+                brushes.AddRange(new RightHexahedralBrushGenerator(veca, vecb, texture).Brushes);
             }
 
             // Side -y
@@ -102,13 +123,38 @@ namespace ICBINGTKR
                     origin.x + (dimensions.x / 2),
                     origin.y - (dimensions.y / 2) + wallThickness,
                     origin.z + (dimensions.z / 2));
-                brushes.Add(new Brush(veca, vecb, tex));
+                brushes.AddRange(new RightHexahedralBrushGenerator(veca, vecb, texture).Brushes);
             }
         }
     }
 
     class RoomGenerator : AssemblyGenerator
     {
+    }
+
+    class TriSoupBrushGenerator : AssemblyGenerator
+    {
+        private IntVec3 spoint, epoint;
+
+        public TriSoupBrushGenerator(IntVec3 veca, IntVec3 vecb)
+            : this(veca, vecb, BrushFace.DEFAULT_TEXTURE) { }
+        public TriSoupBrushGenerator(IntVec3 veca, IntVec3 vecb, Texture texture)
+        {
+            this.spoint = veca;
+            this.epoint = vecb;
+            Utils.fixDirection(ref this.spoint, ref this.epoint);
+
+            Brush b = new Brush();
+
+            b.AddFace(new BrushFace(new IntPlane(this.spoint.x, 0, 0, this.spoint.x, 1, 0, this.spoint.x, 0, 1), texture));
+            b.AddFace(new BrushFace(new IntPlane(this.epoint.x, 0, 0, this.epoint.x, 0, 1, this.epoint.x, 1, 0), texture));
+            b.AddFace(new BrushFace(new IntPlane(0, this.spoint.y, 0, 0, this.spoint.y, 1, 1, this.spoint.y, 0), texture));
+            b.AddFace(new BrushFace(new IntPlane(0, this.epoint.y, 0, 1, this.epoint.y, 0, 0, this.epoint.y, 1), texture));
+            b.AddFace(new BrushFace(new IntPlane(0, 0, this.spoint.z, 1, 0, this.spoint.z, 0, 1, this.spoint.z), texture));
+            b.AddFace(new BrushFace(new IntPlane(0, 0, this.epoint.z, 0, 1, this.epoint.z, 1, 0, this.epoint.z), texture));
+
+            brushes.Add(b);
+        }
     }
 
     abstract class TriSoupGenerator : AssemblyGenerator
